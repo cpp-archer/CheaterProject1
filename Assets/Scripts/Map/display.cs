c09282bcc56fd98206393c32fcd5a872a5e5fa9a
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2026-01-04 02:44:02 +0100

    t

---
+++ Assets/Scripts/Map/display.cs
@@ -0,0 +1,12 @@
+//using UnityEngine;
+
+//public class display : MonoBehaviour
+//{
+//public GameObject pont;
+//    public GameObject pierre;
+//    void Start() { 
+//        GenTerrain();
+    
+//    } void GenTerrain() {
+//        random position = new vector3(point x random sur la map, point y random, point z random); //instantiate l'objet?
+//}

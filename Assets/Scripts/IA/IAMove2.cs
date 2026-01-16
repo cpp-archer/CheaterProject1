ce7c4067a9206b9ed985edd02803cfaf95228dab
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2026-01-16 21:22:33 +0100

    detection

--- Assets/Scripts/IA/IAMove2.cs
+++ Assets/Scripts/IA/IAMove2.cs
@@ -13,11 +13,17 @@ public class IAMove2 : MonoBehaviour
 
 
     public Transform target;
+    public float viewDistance = 10.0f;
+    public float viewAngle = 30.0f;
+
+
+    bool playerDetected = false;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         GotoNextPoint();
+        agent.enabled = true;
     }
 
     void GotoNextPoint()
@@ -30,6 +36,7 @@ public class IAMove2 : MonoBehaviour
 
         destPoint = (destPoint + 1) % points.Length;
         //destPoint = Random.Range(0, points.Length);
+
     }
 
     void Update()
@@ -38,13 +45,54 @@ public class IAMove2 : MonoBehaviour
             GotoNextPoint();
 
 
-        //Vector3 targetDirection = target.position - transform.position;
-        //float angle = Vector3.Angle(targetDirection, transform.forward);
-
-        //if (angle < 5.0f)
-        //    Debug.Log("close");
+        Detection();
+      //DrawViewCone();
 
     }
 
+    private void Detection()
+    {
+
+        //distance
+        float playerDistance= Vector3.Distance(target.position, transform.position);
+
+        //angle
+        Vector3 targetDir = target.position - transform.position;
+        float angle = Vector3.Angle(targetDir, transform.forward);
+        Debug.DrawRay(transform.position, targetDir, Color.red);
+        if (angle < viewAngle)
+        {
+            Debug.Log("Close");
+        }
+       
+        //raycast
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, targetDir, out hit, viewDistance))
+        {
+            if (hit.transform == target)
+            {
+                Debug.Log("I see you");
+                playerDetected = true;
+            }
+        else
+        {
+                Debug.Log("I don't see you");
+         }
+            
+        }
+    }
+    //void DrawViewCone()
+    //{
+    //    int rayCount = 15; 
+    //    float stepAngle = (viewAngle * 2) / rayCount;
+
+    //    for (int i = 0; i <= rayCount; i++)
+    //    {
+    //        float angle = -viewAngle + stepAngle * i;
+    //        Vector3 dir = Quaternion.Euler(0, angle, 0) * transform.forward;
+
+    //        Debug.DrawRay(transform.position, dir * viewDistance, Color.yellow);
+    //    }
+    //}
+
 }
-   

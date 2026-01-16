14f15a84aadcaf622ebb32f0ffb1a0fd54112d69
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2026-01-16 02:45:36 +0100

    ok

--- Assets/Scripts/IA/IAMove2.cs
+++ Assets/Scripts/IA/IAMove2.cs
@@ -2,22 +2,23 @@
 using UnityEngine;
 using UnityEngine.AI;
 using System.Collections;
+using UnityEngine.UIElements;
 
 
 public class IAMove2 : MonoBehaviour
 {
-
     public Transform[] points;
     private int destPoint = 0;
     private NavMeshAgent agent;
-    
+
+
+    public Transform target;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         GotoNextPoint();
     }
-
 
     void GotoNextPoint()
     {
@@ -27,14 +28,23 @@ public class IAMove2 : MonoBehaviour
         //agent.destination = points[destPoint].position;
         agent.SetDestination(points[destPoint].position);
 
-        destPoint = (destPoint + 1)% points.Length;
+        destPoint = (destPoint + 1) % points.Length;
         //destPoint = Random.Range(0, points.Length);
     }
-
 
     void Update()
     {
         if (!agent.pathPending && agent.remainingDistance < 0.5f)
             GotoNextPoint();
+
+
+        //Vector3 targetDirection = target.position - transform.position;
+        //float angle = Vector3.Angle(targetDirection, transform.forward);
+
+        //if (angle < 5.0f)
+        //    Debug.Log("close");
+
     }
+
 }
+   

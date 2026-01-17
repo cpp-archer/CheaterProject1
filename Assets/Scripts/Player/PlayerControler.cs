e7d867fac429b313887d768a64d30a90560ae87f
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2026-01-17 14:21:17 +0100

    walk try

--- Assets/Scripts/Player/PlayerControler.cs
+++ Assets/Scripts/Player/PlayerControler.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,9 +12,10 @@ public class PlayerControler : MonoBehaviour
     private float rotateSpeed = 50f;
     private CharacterController controller;
 
-    public float moveSpeed = 0.5f;
+    // public float moveSpeed = 0.5f;
 
-  
+    private bool rotated = false;
+
     private Vector3 basePosition;
 
     private Animator animator;
@@ -25,7 +26,7 @@ public class PlayerControler : MonoBehaviour
         basePosition = controller.transform.position;
 
         animator = controller.GetComponent<Animator>();
-       // anim.speed = 5;
+        // anim.speed = 5;
     }
 
     void Update()
@@ -33,13 +34,14 @@ public class PlayerControler : MonoBehaviour
         Vector2 stickDirection = moveActionRef.action.ReadValue<Vector2>();
         Vector3 direction = new Vector3(stickDirection.x, 0, stickDirection.y);
 
-       
+
         //comm pour pas que jme deplace en + de l'animation
         //controller.Move(direction * Time.deltaTime * moveSpeed);
 
 
 
-        if (clickRef.action.ReadValue<float>() > 0) { 
+        if (clickRef.action.ReadValue<float>() > 0)
+        {
             Rotate();
         }
         if (direction == Vector3.zero)
@@ -48,53 +50,58 @@ public class PlayerControler : MonoBehaviour
             animator.SetBool("IsBacking", false);
             animator.SetBool("IsRighting", false);
             animator.SetBool("IsLefting", false);
+
+            rotated = false;
         }
 
-        controller.transform.eulerAngles = new Vector3(0, 0, 0);
+        // controller.transform.eulerAngles = new Vector3(0, 0, 0);
 
         if (direction == Vector3.forward)
         {
             //print("move");
             animator.SetBool("IsWalking", true);
-            
+
             basePosition = controller.transform.position;
         }
 
-        else if(direction == Vector3.back)
+        else if (direction == Vector3.back)
         {
-             animator.SetBool("IsBacking", true);
+            animator.SetBool("IsBacking", true);
             basePosition = controller.transform.position;
-            
+
         }
 
         else if (direction == Vector3.right)
         {
             animator.SetBool("IsRighting", true);
-            basePosition = controller.transform.position;
+            //basePosition = controller.transform.position;
 
-            if(clickRef.action.ReadValue<float>() == 0 )
+            if (clickRef.action.ReadValue<float>() == 0 && !rotated)
                 controller.transform.eulerAngles = new Vector3(0, 5, 0); //sinon elle se taille
-            else if (clickRef.action.ReadValue<float>() > 0)
-                Rotate();
+            //else if (clickRef.action.ReadValue<float>() > 0)
+            //    Rotate();
         }
         else if (direction == Vector3.left)
         {
             animator.SetBool("IsLefting", true);
             basePosition = controller.transform.position;
 
-            if (clickRef.action.ReadValue<float>() == 0)
+            if (clickRef.action.ReadValue<float>() == 0 && !rotated)
                 controller.transform.eulerAngles = new Vector3(0, -19, 0);
-            else if(clickRef.action.ReadValue<float>() > 0)
-                Rotate();
+            //else if(clickRef.action.ReadValue<float>() > 0)
+            //    Rotate();j'a
         }
     }
 
-  
+
     private void Rotate()
     {
 
         float mouseRotation = lookActionRef.action.ReadValue<float>();
 
+
         transform.Rotate(Vector3.up * Time.deltaTime * rotateSpeed * mouseRotation);
+
+        rotated = true;
     }
 }

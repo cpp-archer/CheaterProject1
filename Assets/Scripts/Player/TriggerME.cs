1dd3dd16914b2c63de7a2e0b568bae87d2291a43
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2026-01-31 23:50:09 +0100

    cam

--- Assets/Scripts/Player/TriggerME.cs
+++ Assets/Scripts/Player/TriggerME.cs
@@ -6,11 +6,13 @@ public class CamTriggerSwitch : MonoBehaviour
     //public GameObject wall;
     public GameObject cam1;
     public GameObject cam2;
+    public GameObject cam3;
 
     private void Start()
     {
         cam1.SetActive(true);
         cam2.SetActive(false);
+        cam3.SetActive(false);
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -19,6 +21,13 @@ public class CamTriggerSwitch : MonoBehaviour
             cam1.SetActive(!cam1.activeSelf);
             cam2.SetActive(!cam2.activeSelf);
             Debug.Log("switch");
+        }
+
+        if(other.tag== "wall2")
+        {
+            cam1.SetActive(!cam1.activeSelf);
+            cam2.SetActive(!cam2.activeSelf);
+            cam3.SetActive(!cam3.activeSelf);
         }
     }
 }

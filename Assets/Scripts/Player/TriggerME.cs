b864b3b0ad5a5ad89de4a141899ccaf53fb862ab
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2026-02-01 03:18:37 +0100

    3cam

--- Assets/Scripts/Player/TriggerME.cs
+++ Assets/Scripts/Player/TriggerME.cs
@@ -10,24 +10,38 @@ public class CamTriggerSwitch : MonoBehaviour
 
     private void Start()
     {
-        cam1.SetActive(true);
-        cam2.SetActive(false);
-        cam3.SetActive(false);
+        //cam1.SetActive(true);
+        //cam2.SetActive(false);
+        //cam3.SetActive(false);
+        SwitchCam(cam1);
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if (other.tag == "Wall")
+        if (other.tag == "wall2")
         {
-            cam1.SetActive(!cam1.activeSelf);
-            cam2.SetActive(!cam2.activeSelf);
-            Debug.Log("switch");
+        //    cam1.SetActive(!cam1.activeSelf);
+        //    cam2.SetActive(!cam2.activeSelf);
+        //    Debug.Log("switch");
+
+            SwitchCam(cam2);
         }
 
-        if(other.tag== "wall2")
+        if(other.tag== "wall3")
         {
-            cam1.SetActive(!cam1.activeSelf);
-            cam2.SetActive(!cam2.activeSelf);
-            cam3.SetActive(!cam3.activeSelf);
+            //cam1.SetActive(false);
+            //cam2.SetActive(false);
+            //cam3.SetActive(!cam3.activeSelf);
+            SwitchCam(cam3);
         }
+        if(other.tag == "wall1")
+            SwitchCam(cam1);
+    }
+
+    private void SwitchCam(GameObject cam)
+    {
+        cam1.SetActive(false);
+        cam2.SetActive(false);
+        cam3.SetActive(false);
+        cam.SetActive(true);
     }
 }

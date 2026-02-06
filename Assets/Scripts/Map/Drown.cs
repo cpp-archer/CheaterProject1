65dfac300683d10858153295da3074d7abb7f3d4
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2026-02-06 23:51:55 +0100

    fall

--- Assets/Scripts/Map/Drown.cs
+++ Assets/Scripts/Map/Drown.cs
@@ -4,25 +4,21 @@ using UnityEngine.UIElements;
 
 public class Drowns : MonoBehaviour
 {
-
     public GameObject panelLose;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         panelLose.SetActive(false);
     }
 
-
    private IEnumerator untilPanel()
     {   
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.5f);
         panelLose.SetActive(true);
     }
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("player"))
-        {
-           
+        {         
             StartCoroutine(untilPanel());
         }
     }

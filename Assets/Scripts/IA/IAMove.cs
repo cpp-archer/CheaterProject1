6b365c3152e6bd5b5a7f27fae2b1ac584d305a95
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2026-02-15 15:54:39 +0100

    test

--- Assets/Scripts/IA/IAMove.cs
+++ Assets/Scripts/IA/IAMove.cs
@@ -3,16 +3,89 @@ using UnityEngine.AI;
 
 public class IaMove : MonoBehaviour
 {
+    public Transform[] bande1;
+    public Transform[] bande2;
+    public Transform[] bande3;
 
-    public Transform goal;
+    private Transform[] currentBande;
+    private Transform[] currentBalise;
 
-    void Start()
+    private int bandeIndex = 0;
+    private int pointoVisite;
+
+    private NavMeshAgent agent;
+
+    public Transform[] points;
+    private int destPoint = 0;
+
+    public Transform target;
+    public float viewDistance = 20f;
+    public float viewAngle = 45f;
+
+    private bool playerDetected;
+
+    public GameObject panelLoose;
+
+    private void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("player").transform;
 
-        //agent.destination = goal.position;
-        //agent.SetDestination(goal.position);  
+
     }
 
+
+    void ChangeBande()
+    {
+        if (bandeIndex == 0)
+        {
+            currentBande = bande1;
+
+        }
+        else if (bandeIndex == 1)
+        {
+            currentBande = bande2;
+        }
+        else
+        {
+            currentBande = bande3;
+        }
+
+
+
+        pointoVisite = Random.Range(3, 6);
+        currentBalise = new Transform[pointoVisite];
+
+
+
+        for(int i=0; i< pointoVisite; i++)
+        {
+            int rdm = Random.Range(0, currentBande.Length);
+            currentBalise[i] = currentBande[rdm];
+
+        }
+
+        int destPoint = 0;//faut le set zzzzzzz
+
+        //bandeIndex = 
+    }
+
+
+
+    private void GoNextPoint()
+    {
+        if(currentBalise.Length == 0)
+        {
+            return; //jsp frr 
+        }
+
+        agent.SetDestination(currentBalise[destPoint].position);
+
+        destPoint++;
+        if(destPoint >= currentBalise.Length)
+        {
+            ChangeBande();
+        }
+    }
 
 }

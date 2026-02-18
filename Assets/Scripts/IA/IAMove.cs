3e7a13e8d699d301e500a77c27974ee5c7e055d3
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2026-02-18 02:19:13 +0100

    debugs

--- Assets/Scripts/IA/IAMove.cs
+++ Assets/Scripts/IA/IAMove.cs
@@ -115,11 +115,17 @@ public class IAMove : MonoBehaviour
             //points[i + 2] = bande3[Random.Range(0, bande3.Length)];
 
             points[0 + i * 3] = bande1[Random.Range(0, bande1.Length)];
+            Debug.Log("Bande 1, point" +  points[0+i*3].name);
+            
             points[1 + i * 3] = bande2[Random.Range(0, bande2.Length)];
+            Debug.Log("Bande 2, point" + points[1 + i * 3].name);
+
             points[2 + i * 3] = bande3[Random.Range(0, bande3.Length)];
+            Debug.Log("Bande 3, point" + points[2 + i * 3].name);
         }
 
         points[numberByBande * 3] = grimPoint;
+        Debug.Log("grimoirePoint");
 
         for (int i = 0; i < points.Length; i++)
         {
@@ -130,7 +136,6 @@ public class IAMove : MonoBehaviour
             Debug.Log(points[i]);
         }
 
-        
     }
    private void GotoNextPoint()
     {

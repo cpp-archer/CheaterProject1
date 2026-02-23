c0fbeb316c5c038a60fbcf3b4dec75e0e732a573
DJAOUEL Jenna Louisa <jennalouisa.dja150@gmail.com>
2026-02-23 12:06:07 +0100

    menu

--- Assets/Scripts/MenuHome/TutoKeys.cs
+++ Assets/Scripts/MenuHome/TutoKeys.cs
@@ -3,31 +3,31 @@ using UnityEngine.InputSystem;
 
 public class TutoKeys : MonoBehaviour
 {
-    public GameObject tutoP;
-    public InputActionReference openTutoActionRef;
+    public GameObject keysPanel;
+    public InputActionReference openKeysActionRef;
 
     void Start()
     {
-        tutoP.SetActive(true);
+        keysPanel.SetActive(true);
     }
     public void tutoClick() //click souris
     {
-        tutoP.SetActive(!tutoP.activeSelf);
+        keysPanel.SetActive(!keysPanel.activeSelf);
     }
 
     private void OnEnable()
     {
-        openTutoActionRef.action.performed += openTuto;
+        openKeysActionRef.action.performed += openTuto;
     }
 
     private void OnDisable()
     {
-        openTutoActionRef.action.performed -= openTuto;
+        openKeysActionRef.action.performed -= openTuto;
     }
 
     private void openTuto(InputAction.CallbackContext context)
     {
-        tutoP.SetActive(!tutoP.activeSelf);
-        Debug.Log(tutoP.activeSelf);
+        keysPanel.SetActive(!keysPanel.activeSelf);
+        Debug.Log(keysPanel.activeSelf);
     }
 }
